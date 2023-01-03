258bc18bb49cf6ef6c3400ab72892914e0bd33ce
Eric Halle <ehalle@pbs.org>
2023-01-03 15:12:56 -0500

    IAdapterClient -> AdapterClient

--- src/main/java/org/pbs/sgladapter/adapter/AdapterClient.java
+++ src/main/java/org/pbs/sgladapter/adapter/AdapterClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-public interface IAdapterClient {
+public interface AdapterClient {
 
   @PostMapping("/v1/files/")
   public String restore(@RequestBody String payload);
